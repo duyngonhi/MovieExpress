fcf6af204c237be971eb04e1669ab74dbc0faf9a
NgoVanDuy <ngo.van.duy@framgia.com>
2017-04-07 17:55:41 +0700

    init Structure

---
+++ src/main/java/vn/framgia/service/ICityService.java
@@ -0,0 +1,7 @@
+package vn.framgia.service;
+
+import java.io.Serializable;
+
+public interface ICityService extends Serializable {
+
+}
---
+++ src/main/java/vn/framgia/service/impl/CityServiceImpl.java
@@ -0,0 +1,7 @@
+package vn.framgia.service.impl;
+
+import vn.framgia.service.ICityService;
+
+public class CityServiceImpl implements ICityService {
+	
+}
